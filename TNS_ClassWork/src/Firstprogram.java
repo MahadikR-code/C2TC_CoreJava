dc7db63e47d86272b96af5631a8b581e4b70cf5c
Rohit Mahadik <rohitmahadik3503@gmail.com>
2022-01-30 21:07:56 +0530

    Delete TNS_ClassWork directory

--- TNS_ClassWork/src/Firstprogram.java
+++
@@ -1,8 +0,0 @@
-
-public class Firstprogram {
-
-	public static void main(String[] args) {
-		System.out.println("Hello This is first Program");
-	}
-
-}
--- TNS_ClassWork/src/WrapperExample2.java
+++
@@ -1,8 +0,0 @@
-
-public class WrapperExample2 
-{
- public static void main(String[] args) 
- 	{
-	
- 	}
-}
