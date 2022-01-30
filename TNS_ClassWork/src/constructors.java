485131aa94d03f5739e93b3cb533f2b8545da633
Rohit Mahadik <rohitmahadik3503@gmail.com>
2022-01-30 21:40:01 +0530

    Add files via upload

---
+++ TNS_ClassWork/src/constructors.java
@@ -0,0 +1,17 @@
+
+public class constructors{
+	constructors(){
+		
+		System.out.println("Bike is Created");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       constructors a= new constructors();
+       constructors b= new constructors();
+       constructors c= new constructors();
+       constructors d= new constructors();
+       constructors e= new constructors();
+	}
+
+}
---
+++ TNS_ClassWork/src/switch_case.java
@@ -0,0 +1,25 @@
+
+public class switch_case {
+
+	public static void main(String[] args) 
+	{
+	int x=100;
+	
+	switch(x)
+	{
+	default:
+		System.out.println("default");
+		break;
+		
+	case 0:
+		System.out.println(0);
+		break;
+	case 1:
+		System.out.println(1);
+	case 2:
+		System.out.println(2);		
+	}
+	
+	}
+
+}
